414cbd2e3ce9a2e14a8517f05d61faab770b2a42
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2021-03-12 09:18:19 +0000

    added more comments

--- p01-src/modules/quickSelect.py
+++ p01-src/modules/quickSelect.py
@@ -6,17 +6,25 @@ import math
 '''
 def QuickSelect(items, item_index):
 
+    # Checks if the is list has items in it
     if items is None or len(items) < 1:
         return None
+    # Checks the Index is not greater than the length of the list
     if item_index < 0 or item_index > len(items) - 1:
         raise IndexError()
+    #Runs the quick select algorithm
     return select(items, 0, len(items) - 1, item_index)
 
+
+'''
+@param lst the list being partitioned
+@param l the begining index of the left side
+@param r the begining index of the right side
+@param index the k-th biggest index we are looking for
+'''
 def select(lst, l, r, index):
-    # print("*** ", lst[l:r+1], " ***")
     # base case
     if r == l:
-        # print(lst[l:r+1])
         return r
 
     # choose random pivot
@@ -26,31 +34,23 @@ def select(lst, l, r, index):
     # partition
     # Last available Space
     i = l
-    # print("Pivot: ", lst[l])
     for j in range(l+1, r+1):
         if lst[j] < lst[i]:
-            # print("j: ", lst[j], " i: ", lst[i])
-            # print("First: ", lst[l:r + 1])
             if j > i + 1:
                 lst[i], lst[i + 1] = lst[i + 1], lst[i]
-            # print("Second: ", lst[l:r + 1])
             lst[j], lst[i] = lst[i], lst[j]
-            # print("Third: ", lst[l:r+1])
-            # print("Changes: ", lst)
             i += 1
 
     # recursively partition one side only
     if index == i:
-        # print(i)
-        # print("index: ", i)
-        # print("index value: ", lst[i])
         return i
     elif index < i:
         return select(lst, l, i-1, index)
     else:
         return select(lst, i+1, r, index)
-#Avoid worse cases of sorted array.
-#Reproducable unlike using random index to get result.
+
+# Avoid worse cases of sorted array.
+# Reproducable unlike using random index to get result.
 def choosePivot(collection):
     return math.floor(len(collection)/2)
 
